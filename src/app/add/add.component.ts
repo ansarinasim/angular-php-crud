f372a444ba4c253fc34bdb1c2a50ee05091df426
ansarinasim <112812721+ansarinasim@users.noreply.github.com>
2024-06-06 14:00:54 +0530

    Add files via upload

---
+++ src/app/add/add.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+@Component({
+  selector: 'app-add',
+  templateUrl: './add.component.html',
+  styleUrls: ['./add.component.css']
+})
+export class AddComponent implements OnInit {
+
+   addForm : any;
+
+  constructor(private formBuilder: FormBuilder) { 
+
+   
+  }
+  ngOnInit() {
+    
+
+    this.addForm = this.formBuilder.group({
+      sId: [],
+      fName: ['', Validators.required],
+      lName: ['', [Validators.required, Validators.maxLength(9)]],
+      email: ['', [Validators.required, Validators.maxLength(30)]]
+    });
+    }
+
+    onSubmit() {
+      console.log(this.addForm.value);
+  }
+
+}
